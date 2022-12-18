b4b4c23e0e71bdf17d4b67c32b64e2643e3e48c7
Shashank <mishra.shashankofficial06@gmail.com>
2022-12-18 21:07:55 +0530

    Added: Welcome message & README.md file

---
+++ README.md
@@ -0,0 +1,2 @@
+This is address book management system project. Where we can add & remove the contacts from the address book by taking user
+input.
---
+++ src/day9assignment/addressbook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package day9assignment.addressbook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program.");
+    }
+}
